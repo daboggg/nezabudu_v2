329e696d3883aab7952529c49471152e334d47c8
daboggg <vzinin@list.ru>
2024-02-24 09:32:29 +0300

    fix from_datetime_to_str.py

--- bot/utils/from_datetime_to_str.py
+++ bot/utils/from_datetime_to_str.py
@@ -1,12 +1,18 @@
 from datetime import datetime
 
-months = ["","января","февраля","марта","апреля",
-          "мая","июня","июля","августа","сентября",
-          "октября","ноября","декабря",]
+months = ["", "января", "февраля", "марта", "апреля",
+          "мая", "июня", "июля", "августа", "сентября",
+          "октября", "ноября", "декабря", ]
 
-day_of_week = ["в понедельник","во вторник","в среду","в четверг",
-               "в пятницу","в субботу","в воскресенье",]
+day_of_week = ["в понедельник", "во вторник", "в среду", "в четверг",
+               "в пятницу", "в субботу", "в воскресенье", ]
+
 
 def datetime_to_str(date: datetime):
-    return f"{date.day} {months[date.month]} {date.year} г. ({day_of_week[date.weekday()]}) в {date.hour}:{date.minute}"
-
+    hour = date.hour
+    minute = date.minute
+    if hour < 10:
+        hour = f'0{hour}'
+    if minute <10:
+        minute = f'0{minute}'
+    return f"{date.day} {months[date.month]} {date.year} г. ({day_of_week[date.weekday()]}) в {hour}:{minute}"
